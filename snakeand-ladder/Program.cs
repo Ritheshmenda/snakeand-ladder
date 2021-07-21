acaac03e57b2f9e78d290e31f43b1abfd11d59e9
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-21 23:14:11 +0530

    [rithesh]added:introduction

---
+++ snakeand-ladder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace snakeandladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            snakeandladder sl = new snakeandladder();
+            sl.introduction();
+        }
+    }
+}
---
+++ snakeand-ladder/snakeandladder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snakeandladder
+{
+    class snakeandladder
+    {
+        public void introduction()
+        {
+            Console.WriteLine("welcome to snake and ladder problem");
+        }
+    }
+}
