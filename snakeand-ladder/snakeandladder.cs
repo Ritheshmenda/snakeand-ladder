51cb0b7a3cdef0bcdb54658b9d4dc69118ae37ff
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-22 18:40:21 +0530

    [rithesh]added:threecases

--- snakeand-ladder/snakeandladder.cs
+++ snakeand-ladder/snakeandladder.cs
@@ -9,9 +9,28 @@ namespace snakeandladder
             
         public void gamestarts()
         {
+            int initial = 0;
             Random random = new Random();
             int firstroll = random.Next(1, 7);
-            Console.WriteLine("player's new position is:" + firstroll);
+            int option = random.Next(1, 3);
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Ladder");
+                    Console.WriteLine("player position is :" + firstroll);
+                    break;
+                case 2:
+                    Console.WriteLine("roll value is" + firstroll);
+                    Console.WriteLine("Snake");
+                    initial -= firstroll;
+                    if (initial < 0)
+                        initial = 0;
+                    Console.WriteLine("initial player position that is  :" + initial);
+                    break;
+                default:
+                    Console.WriteLine("no play");
+                    break;
+            }
         }
     }
 }
