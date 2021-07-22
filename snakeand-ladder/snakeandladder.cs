b2c2d9b7099e7a317c9037d0cbbbd977009ce6a0
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-22 20:14:27 +0530

    [rithesh]added:tillthe100

--- snakeand-ladder/snakeandladder.cs
+++ snakeand-ladder/snakeandladder.cs
@@ -10,26 +10,33 @@ namespace snakeandladder
         public void gamestarts()
         {
             int initial = 0;
-            Random random = new Random();
-            int firstroll = random.Next(1, 7);
-            int option = random.Next(1, 3);
-            switch (option)
+            while (initial < 100)
             {
-                case 1:
-                    Console.WriteLine("Ladder");
-                    Console.WriteLine("player position is :" + firstroll);
-                    break;
-                case 2:
-                    Console.WriteLine("roll value is" + firstroll);
-                    Console.WriteLine("Snake");
-                    initial -= firstroll;
-                    if (initial < 0)
-                        initial = 0;
-                    Console.WriteLine("initial player position that is  :" + initial);
-                    break;
-                default:
-                    Console.WriteLine("no play");
-                    break;
+                Random random = new Random();
+                int firstroll = random.Next(1, 7);
+                int option = random.Next(1, 3);
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Ladder");
+                        Console.WriteLine("player position is :" + firstroll);
+                        initial += firstroll;
+                        if (initial > 100)
+                            initial -= firstroll;
+                        Console.WriteLine("initial player position that is  :" + initial);
+                        break;
+                    case 2:
+                        Console.WriteLine("roll value is" + firstroll);
+                        Console.WriteLine("Snake");
+                        initial -= firstroll;
+                        if (initial < 0)
+                            initial = 0;
+                        Console.WriteLine("initial player position that is  :" + initial);
+                        break;
+                    default:
+                        Console.WriteLine("no play");
+                        break;
+                }
             }
         }
     }
